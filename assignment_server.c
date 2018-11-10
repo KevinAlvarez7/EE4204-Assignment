630708444d02d9e17992ea90a7c7498c70c7a697
kaiyan <kaiyan.yeo@gmail.com>
2018-11-10 22:20:00 +0800

    UDP transmission of a large file. ACK at end of receive.

---
+++ assignment_server.c
@@ -0,0 +1,89 @@
+/**********************************
+udp_ser.c: the source file of the server in udp transmission
+***********************************/
+
+
+#include "headsock.h"
+
+void str_ser(int sockfd);                                                        // transmitting and receiving function
+
+int main(void)
+{
+	int sockfd, con_fd, ret;
+	struct sockaddr_in my_addr;
+	struct sockaddr_in their_addr;
+	int sin_size;
+
+//	char *buf;
+	pid_t pid;
+
+	sockfd = socket(AF_INET, SOCK_DGRAM, 0);          //create socket
+	if (sockfd<0)
+	{
+		printf("error in socket!");
+		exit(1);
+	}
+	
+	my_addr.sin_family = AF_INET;
+	my_addr.sin_port = htons(MYUDP_PORT);
+	my_addr.sin_addr.s_addr = INADDR_ANY;//inet_addr("172.0.0.1");
+	bzero(&(my_addr.sin_zero), 8);
+	ret = bind(sockfd, (struct sockaddr *) &my_addr, sizeof(struct sockaddr));                //bind socket
+	if (ret<0)
+	{
+		printf("error in binding");
+		exit(1);
+	}
+	
+	while (1)
+	{
+		printf("waiting for data\n");
+		str_ser(con_fd);                                          //receive packet and response
+	}
+	close(sockfd);
+	exit(0);
+}
+FILE *fp, int sockfd, struct sockaddr *addr, int addrlen, long *len
+void str_ser(int sockfd, struct sockaddr *addr, int addrlen)
+{
+	char buf[BUFSIZE];
+	FILE *fp;
+	char recvs[DATALEN];
+	struct ack_so ack;
+	int end, n = 0;
+	long lseek=0;
+	end = 0;
+	
+	printf("receiving data!\n");
+
+	while(!end)
+	{
+		if ((n= recvfrom(sockfd, &recvs, DATALEN, 0, addr, addrlen))==-1)                                   //receive the packet
+		{
+			printf("error when receiving\n");
+			exit(1);
+		}
+		if (recvs[n-1] == '\0')									//if it is the end of the file
+		{
+			end = 1;
+			n --;
+		}
+		memcpy((buf+lseek), recvs, n);
+		lseek += n;
+	}
+	ack.num = 1;
+	ack.len = 0;
+	if ((n = sendto(sockfd, &ack, 2, 0, addr, addrlen))==-1)
+	{
+			printf("send error!");								//send the ack
+			exit(1);
+	}
+	if ((fp = fopen ("fileReceive.txt","wt")) == NULL)
+	{
+		printf("File doesn't exit\n");
+		exit(0);
+	}
+	fwrite (buf , 1 , lseek , fp);					//write data into file
+	fclose(fp);
+	printf("a file has been successfully received!\nthe total data received is %d bytes\n", (int)lseek);
+}
